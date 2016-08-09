b24f51d8a71096b02ced0bd43d22b536ee27ef75
Nikhil <nikhilrampal15@gmail.com>
2016-08-08 17:05:19 -0700

    implemented k-means in python

--- kmeans.py
+++ kmeans.py
@@ -1,35 +1,40 @@
 import pandas as pd
 import numpy as np
 import matplotlib
-matplotlib.use('TkAgg') # Workaround of an error: Python is not installed as a framework
-from sets import Set
+matplotlib.use('TkAgg')
+# Workaround of an error: Python is not installed as a framework
 import random
-
-def initialize_centroid(data, k):
-    centroids = Set()
-    while len(centroids) != k:
-        centroid = random.choice(data)
-        centroids.add(centroid)
-    return list(centroids)
-
-def assign_cluster(centroids, data):
-    data = data[0:3]
-    for item in data:
-        min_distance = float('inf')
-        print "item = {}".format(item)
-        for idx, centroid in enumerate(centroids):
-            print "idx = {}, centroid = {}".format(idx, centroid)
-            distance = pow((int(centroid) - int(item)), 2)
-            if distance < min_distance:
-                min_distance = distance
-                cluster_idx = idx
-                print "cluster_idx = {}".format(cluster_idx)
-
-    return
-
-
-def kmeans():
+k=5
+def centroids(house_data):
+    #takes in housing data parameter
+    centroid = []
     pass
+    for x in range(k):
+        #loop from 0 to 5
+        centroid.append(random.randint(1,len(house_data)))
+        #adds a random integer to the list
+    return centroid
+    #return the array of random points
+
+
+def cluster(centroids,house_data):
+    shortest_distance =[]
+    #create an list that compares distance from centeroid to house
+    pass
+    for x in range(k):
+        #loop from 0 to 5
+        houses = house_data[x]
+        centroid = centroids[x]
+        if houses > centroid:
+            shortest_distance.append([houses-centroids**2,centroids])
+            #adds RMSE distance to list along with centroid location
+        else:
+            shortest_distance.append([centroids-houses**2,centroids])
+    shortest_distance.sort(key=lambda z: z[0])
+    #sorts zeroth index of list inside list which is the distance
+    return shortest_distance[0][1]
+    #returns the cluster that is closest to the home
+
 
 if __name__ == '__main__':
     city = "redwood-city-ca"
