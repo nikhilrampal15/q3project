acf5bda43081dfab7f19a6afb65ea822e9420744
Nikhil <nikhilrampal15@gmail.com>
2016-08-10 21:13:21 -0700

    clustering works properly

--- kmeans.py
+++ kmeans.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import numpy as np
-import plotly.plotly as py
-import plotly.graph_objs as go
 import random
 from read_csvfile import read_csvfile
 k = 5
-city = "redwood-city-ca"
+city = "san-francisco-ca"
 filename = "data/propertyInfo/{}.csv".format(city)
 data = read_csvfile(filename)
 data["cluster"] = -1
@@ -23,47 +21,63 @@ def centroids(house_data):
     return centroid
     # return the array of random points
 
-
+import copy
 def cluster(centroids, house_data):
     shortest_distance = []
     # create an list that compares distance from centeroid to house
     pass
+    houses = house_data['zestimate']
     for x in range(len(centroids)):
         # loop from 0 to 5
         centroid = centroids[x]
         #print(type(centroid))
-        # print(centroids[x])
-        houses = house_data['zestimate']
+        #print(centroid)
         # print(houses)
         for idx, y in enumerate(houses):
+                offset = len(houses) * x
+                #print(idx, len(houses))
                 home = houses[idx]
                 if home > centroid:
                     shortest_distance.append([home-centroid, centroid, y])
-                    house_data.loc[idx, 'cluster'] = centroid
+                    house_data.loc[idx + offset, 'cluster'] = centroid
+
             # adds distance to list along with centroid location
                 else:
                     shortest_distance.append([centroid-home, centroid, y])
-                    house_data.loc[idx, 'cluster'] = centroid
+                    house_data.loc[idx + offset, 'cluster'] = centroid
     shortest_distance.sort(key=lambda a: a[0])
-
     # sorts zeroth index of list inside list which is the distance
+    """
+    here we create the convergence method
+    """
 
+
+    shortest_distance.sort(key=lambda a: a[1])
+    # for i in shortest_distance:
+    #     centroid_sort = shortest_distance.sort(key=lambda a: a[1])
     return shortest_distance
     # returns the cluster that is closest to the home
 
 
 
+
+
+
 answer = centroids(data)
 #print(answer)
-#print(cluster(answer, data))
-#print(len(data))
+cluster(answer, data)
+#print(data)
+
 # for item in myData:
 # print(item)
-# print(data)
+#print(data)
+print(data['cluster'])
+# for item in data['cluster']:
+#     print(item)
 
-pd.set_option('display.max_rows',len(data['zestimate']))
+pd.set_option('display.max_rows', len(data['zestimate']))
 x = data['zestimate']
-print (x)
+#print (x)
 pd.set_option('display.float_format', lambda x: '%.0f' % x)
-print (x)
+#print (x)
 #print(data['zestimate'])
